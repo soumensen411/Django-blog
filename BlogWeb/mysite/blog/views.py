ad93855dbfe2e1293216f2df4af0acde4d8a355a
Soumen sen <144879914+soumensen411@users.noreply.github.com>
2025-03-27 07:38:27 +0000

    updated

--- BlogWeb/mysite/blog/views.py
+++ BlogWeb/mysite/blog/views.py
@@ -3,3 +3,13 @@ from django.http.response import HttpResponse
 # Create your views here.
 def homepage(request):
     return render(request,'base.html')
+def home(request):
+    return render(request,'index.html')
+def menu(request):
+    return render(request,'menu.html')
+def order(request):
+    return render(request,'order.html')
+def about(request):
+    return render(request,'about.html')
+def feedback(request):
+    return render(request,'feedback.html')
--- BlogWeb/mysite/mysite/urls.py
+++ BlogWeb/mysite/mysite/urls.py
@@ -18,8 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',views.homepage),
+    path('',views.homepage, name= 'home'),
+    path('home',views.home, name='home'),
+    path('menu',views.menu, name = 'menu'),
+    path('order',views.order, name = 'order'),
+    path('about',views.about, name = 'about'),
+    path('feedback',views.feedback, name = 'feedback'),
 ]
